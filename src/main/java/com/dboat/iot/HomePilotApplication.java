3fac33f296493859996d91c688964d4b304b9e60
D_boat <1390927956@qq.com>
2026-08-08 14:32:27 +0800

    feature: project started flag、log manage

--- src/main/java/com/dboat/iot/HomePilotApplication.java
+++ src/main/java/com/dboat/iot/HomePilotApplication.java
@@ -1,12 +1,20 @@
 package com.dboat.iot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class HomePilotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HomePilotApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(HomePilotApplication.class, args);
+
+        log.info("========================================");
+        log.info("\uD83C\uDF89 Home‑Pilot service started successfully");
+        log.info("✅ Total beans: {}", context.getBeanDefinitionCount());
+        log.info("========================================");
     }
 }
